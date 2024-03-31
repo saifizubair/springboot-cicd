3759a72f71ceeb9102eda722fa2dda1436cecdf1
mohammadzubair01 <mohammad.zubair01@nagarro.com>
2024-03-31 16:37:49 +0530

    Commit successful

---
+++ src/main/java/controller/HelloController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	
+	@GetMapping("/welcome")
+	public String getMessage() {
+		return "Welcome to Hello world";
+	}
+}
